a6241bccc16c6ffe4f044acdb64fc9c799e74de0
John Rojas <jrojas1202@gmail.com>
2023-11-22 09:13:06 -0500

    profile fixes and added log out and change profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,14 +1,24 @@
 import "../Profile/Profile.css";
 import SideBar from "./SideBar/SideBar";
 import ClothesSection from "./ClothesSection/ClothesSection";
-function Profile({ clothingItems, onSelectCard, onOpenModal }) {
+function Profile({
+  clothingItems,
+  onSelectCard,
+  onOpenModal,
+  onEditProfile,
+  onLogout,
+  isLoggedIn,
+  onCardLike,
+}) {
   return (
     <main className="profile">
-      <SideBar />
+      <SideBar onEditProfile={onEditProfile} onLogout={onLogout} />
       <ClothesSection
         clothingItems={clothingItems}
         onSelectCard={onSelectCard}
         onOpenModal={onOpenModal}
+        isLoggedIn={isLoggedIn}
+        onCardLike={onCardLike}
       />
     </main>
   );
--- src/components/Profile/SideBar/SideBar.js
+++ src/components/Profile/SideBar/SideBar.js
@@ -1,13 +1,43 @@
 import "./SideBar.css";
-import avatar from "../../../images/avatar.svg";
+import { CurrentUserContext } from "../../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-const SideBar = () => {
+const SideBar = ({ onEditProfile, onLogout }) => {
+  const currentUser = useContext(CurrentUserContext);
+  const currentAvatar = currentUser.avatar !== "" ? true : false;
+
   return (
     <div className="sidebar">
-      <div>
-        <img className="sidebar__avatar" src={avatar} alt="Avatar icon" />
+      <div className="sidebar__profile-info">
+        {currentAvatar ? (
+          <img
+            className="sidebar__avatar"
+            src={currentUser.avatar}
+            alt="Avatar icon"
+          />
+        ) : (
+          <p className="sidebar__avatar-default">
+            {currentUser.name[0].toUpperCase()}
+          </p>
+        )}
+        <p className="sidebar__name">{currentUser.name}</p>
       </div>
-      <p className="sidebar__name">John Rojas</p>
+      <div className="sidebar__profile-manager">
+        <button
+          className="sidebar__edit-btn"
+          type="button"
+          onClick={onEditProfile}
+        >
+          Change Profile Data
+        </button>
+        <button
+          className="sidebar__logout-btn"
+          type="button"
+          onClick={onLogout}
+        >
+          Log out
+        </button>
+      </div>
     </div>
   );
 };
